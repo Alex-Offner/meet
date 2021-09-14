e2b776d08b334c9ff54cce472d2b533543aa9ea4
Alex-Offner <80695644+Alex-Offner@users.noreply.github.com>
2021-09-14 10:00:09 -0700

    removed +

--- src/api.js
+++ src/api.js
@@ -45,7 +45,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = 'https://dat3m21my8.execute-api.us-west-2.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = 'https://dat3m21my8.execute-api.us-west-2.amazonaws.com/dev/api/get-events/' + token;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
@@ -80,7 +80,7 @@ export const getAccessToken = async () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const { access_token } = await fetch(
-        'https://dat3m21my8.execute-api.us-west-2.amazonaws.com/dev/api/token' + '/' + encodeCode
+        'https://dat3m21my8.execute-api.us-west-2.amazonaws.com/dev/api/token/' + encodeCode
     )
         .then((res) => {
             return res.json();
